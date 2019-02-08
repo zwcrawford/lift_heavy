9e48d96280eb5420c66227d62b53d7177f1dd36d
Zac Crawford <zwcrawford@gmail.com>
2019-02-08 11:11:12 -0600

    got both dropdowns to populate

--- src/components/exercise/ExerciseCard.js
+++ src/components/exercise/ExerciseCard.js
@@ -16,51 +16,56 @@ import "./Exercise.css";
 
 export default class ExerciseCard extends Component {
   render() {
+    // console here
+    console.log("", this.props);
+
     return (
-      <div key={this.props.exercise.id} className="exerciseCard">
-        <div className="card-body">
-          <h5 className="card-title">
-            Exercise: {this.props.exercise.exerciseName}
-          </h5>
-          <label>
-            Body Group: {this.props.exercise.bodyCategory ? this.props.exercise.bodyCategory.categoryName : ""}
-          </label>
-          <br />
-          <label>
-            Equipment: {this.props.exercise.equipmentName}
-          </label>
-          <br />
-          <label>
-            # of Sets: {this.props.exercise.sets}
-          </label>
-          <br />
-          <label>
-            # of Reps: {this.props.exercise.reps}
-          </label>
-          <br />
-          <label>
-            Weigh / Set: {this.props.exercise.weightLifted}
-          </label>
-          <br />
-          <label>
-            Notes: {this.props.exercise.notes}
-          </label>
-          <div className="editButton">
-            <button
-              href={`/exercises/${this.props.exercise.id}/edit`}
-              onClick={() =>
-                this.props.updateExercise(this.props.exercise.id, this.props.exercise)}
+      <React.Fragment>
+        <div key={this.props.exercise.id} className="exerciseCard">
+          <div className="card-body">
+            <h5 className="card-title">
+              Exercise: {this.props.exercise.exerciseName}
+            </h5>
+            <label>
+              Body Group: {this.props.exercise.bodyCategory ? this.props.exercise.bodyCategory.categoryName : ""}
+            </label>
+            <br />
+            <label>
+              Equipment: {this.props.exercise.equipmentType ? this.props.exercise.equipmentType.equipmentName : "wtf"}
+            </label>
+            <br />
+            <label>
+              # of Sets: {this.props.exercise.sets}
+            </label>
+            <br />
+            <label>
+              # of Reps: {this.props.exercise.reps}
+            </label>
+            <br />
+            <label>
+              Weigh / Set: {this.props.exercise.weightLifted}
+            </label>
+            <br />
+            <label>
+              Notes: {this.props.exercise.notes}
+            </label>
+            <div className="editButton">
+              <button
+                href={`/exercises/${this.props.exercise.id}/edit`}
+                onClick={() =>
+                  this.props.updateExercise(this.props.exercise.id, this.props.exercise)}
+                  className="card-button"
+              >Edit</button>
+            </div>
+              <div className="deleteButton">
+              <button
                 className="card-button"
-            >Edit</button>
-          </div>
-            <div className="deleteButton">
-            <button
-              onClick={() => this.props.deleteExercise(this.props.exercise.id)}
-              className="card-button"
-            >Delete</button>
+                onClick={() => {this.props.deleteExercise(this.props.exercise.id)}}
+              >Delete</button>
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/modules/ExerciseManager.js
+++ src/modules/ExerciseManager.js
@@ -5,7 +5,7 @@ export default {
     return fetch(`${remoteURL}/exercises/${id}`).then(e => e.json());
   },
   getAll() {
-    return fetch(`${remoteURL}/exercises?_expand=bodyCategory&_expand=equipmentName`).then(e => e.json());
+    return fetch(`${remoteURL}/exercises?_expand=bodyCategory&_expand=equipmentType`).then(e => e.json());
   },
   post(exercise) {
     return fetch(`${remoteURL}/exercises`, {
