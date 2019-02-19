9aceb71cc46e2d0e48339e41208c9d836d3428e2
Zac Crawford <zwcrawford@gmail.com>
2019-02-19 10:04:56 -0600

    added image at top, borders and centered content

--- src/components/exercise/ExerciseCard.js
+++ src/components/exercise/ExerciseCard.js
@@ -53,27 +53,27 @@ export default class ExerciseCard extends Component {
             <h5 className="card-title">
               Exercise: {this.props.exercise.exerciseName}
             </h5>
-            <label>
+            <label className="card-label">
               Body Group: {this.props.exercise.bodyCategory ? this.props.exercise.bodyCategory.categoryName : ""}
             </label>
             <br />
-            <label>
+            <label className="card-label">
               Equipment: {this.props.exercise.equipmentType ? this.props.exercise.equipmentType.equipmentName : ""}
             </label>
             <br />
-            <label>
+            <label className="card-label">
               # of Sets: {this.props.exercise.sets}
             </label>
             <br />
-            <label>
+            <label className="card-label">
               # of Reps: {this.props.exercise.reps}
             </label>
             <br />
-            <label>
+            <label className="card-label">
               Weigh / Set: {this.props.exercise.weightLifted}
             </label>
             <br />
-            <label>
+            <label className="card-label">
               Notes: {this.props.exercise.notes}
             </label>
             <div className="links">
